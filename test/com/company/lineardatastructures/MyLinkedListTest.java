6f0a1d52be2fd822fec54f974eb0f4b73500858e
Mohammad Noman <mohammad.noman@live.in>
2021-03-17 15:19:58 +0530

    first unit test for linked list insert

---
+++ test/com/company/lineardatastructures/MyLinkedListTest.java
@@ -0,0 +1,17 @@
+package com.company.lineardatastructures;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyLinkedListTest {
+
+    @Test
+    void insertItem(){
+        MyLinkedList<Integer> list = new MyLinkedList<>();
+        list.insert(21);
+        list.insert(321);
+        assertNotNull(list);
+        assertEquals(2, list.size());
+    }
+}
